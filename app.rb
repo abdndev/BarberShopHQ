8b82cc498a25faed24ac59fc0e2606b144251398
ABDNdev <igor@abidin.ru>
2019-10-24 14:10:36 +0300

    Modify several files

--- app.rb
+++ app.rb
@@ -12,7 +12,32 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
-	@barbers = Barber.order "created_at DESC"
+	
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+   	@username = params[ :username]
+	@phone     = params[ :phone]
+	@datetime = params[ :datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	
+	# или вывод сообщения на пустой странице
+	erb "<h2>Спасибо! вы записались!</h2>"
+	
+end
+
+get '/contacts' do
+	erb :contacts
 end
